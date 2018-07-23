c47fb2038bd87e47696e849b78c118e493f7c6a7
Felix JC <felixjcalvo@gmail.com>
2018-07-23 17:16:30 +0200

    Signed-off-by: Felix JC <felixjcalvo@gmail.com>

--- src/code/Game.java
+++ src/code/Game.java
@@ -2,16 +2,34 @@ package code;
 
 public class Game {
 	
-	private int _score = 0;
+	private int current_roll = 0;
+	private int[] rolls = new int[21];
 	
-	public void roll(int quantity_pins)
+	public void Roll(int quantity_pins)
 	{
-		_score += quantity_pins;
+		rolls[current_roll] = quantity_pins;
+		current_roll++;
 	}
 	
 	public int scoredPoints()
 	{
-		return _score;
+		int roll_num = 0, score = 0;
+		
+		for(int frame = 0; frame <10; frame++)
+		{
+			if(rolls[roll_num] + rolls[roll_num + 1] == 10)
+			{
+				score += 10 + rolls[roll_num + 2];
+			}
+			else
+			{
+				score += rolls[roll_num] + rolls[roll_num + 1];
+			}
+			roll_num += 2;
+		}		
+		return score;
 	}
+	
+
 
 }
--- src/test/TestBowling.java
+++ src/test/TestBowling.java
@@ -7,14 +7,6 @@ import org.junit.Test;
 
 import code.Game;
 
-// Important
-// When you play bowling you have 2 opportunities or
-// if you roll more than 7 pins in your turn you make a spare / 
-// or even a Strike if you roll all, so it means
-// 2 tries in every turn, in 10 turns means 20 intents, without doing spares or strikes
-// Test the game, test the intents points
-
-
 public class TestBowling {
 	
 	private Game bowling;
@@ -39,12 +31,24 @@ public class TestBowling {
 		assertEquals(20, bowling.scoredPoints());
 	}
 	
+	@Test
+	public void OneSpareReturns() 
+	{
+		// Turn
+		bowling.Roll(5); // first roll
+		bowling.Roll(5); // second roll = spare
+		bowling.Roll(3);
+		
+		RollMany(17, 0);
+		
+		assertEquals(16, bowling.scoredPoints());
+	}
+	
 	public void RollMany(int rolls, int pins)
 	{
 		for(int i = 0; i <rolls; i++)
 		{
-			bowling.roll(pins);
+			bowling.Roll(pins);
 		}
-	}
-	
+	}	
 }
